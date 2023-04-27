556ecb941462c753743761143c6fec2cb96ab3b8
vikas <vikasisgen@gmail.com>
2023-04-27 15:57:58 +0530

    call2

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,8 @@
 import '@/styles/globals.scss'
 import type { AppProps } from 'next/app'
 import { Exo_2 } from 'next/font/google';
-
+import { Analytics } from '@vercel/analytics/react'
+;
 const font = Exo_2({
   subsets:['latin'],
   weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
@@ -13,6 +14,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <main className={font.className}>
       <Component {...pageProps} />
+      <Analytics />
     </main>
   )
 }
