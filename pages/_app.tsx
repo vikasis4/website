d8829c91fec4a1622b7a3394a85b977ecccecfee
vikas <vikasisgen@gmail.com>
2023-05-11 12:57:46 +0530

    final

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,9 @@
 import '@/styles/globals.scss'
 import type { AppProps } from 'next/app'
 import { Exo_2 } from 'next/font/google';
-import { Analytics } from '@vercel/analytics/react'
-;
+import { Analytics } from '@vercel/analytics/react';
 const font = Exo_2({
-  subsets:['latin'],
+  subsets: ['latin'],
   weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
 })
 // weight:'400'
@@ -13,8 +12,8 @@ const font = Exo_2({
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <main className={font.className}>
-      <Component {...pageProps} />
-      <Analytics />
+        <Component {...pageProps} />
+        <Analytics />
     </main>
   )
 }
---
+++ pages/payment/show.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function show() {
+  return (
+    <>
+    <div style={{height:'100vh', flexDirection:'column', width:'100vw',gap:'2rem', display: 'flex', justifyContent:'center', alignItems: 'center'}}>
+        <h1 style={{color:'#5A4FCF'}}>RankBoost</h1>
+        <h2>Wishes you best for your prep</h2>
+    </div>
+    </>
+  )
+}
+
+export default show
